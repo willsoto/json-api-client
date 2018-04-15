3ddf70a952ff86001de3f1cf1e484ef37e6eca0a
Will Soto <will.soto9@gmail.com>
2018-04-14 21:00:17 -0400

    feat(rollup): generate es bundle along with umd
    Signed-off-by: Will Soto <will.soto9@gmail.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,42 @@
-import path from "path";
+import path from 'path';
 
-import typescript from "rollup-plugin-typescript2";
-import filesize from "rollup-plugin-filesize";
+import typescript from 'rollup-plugin-typescript2';
+import filesize from 'rollup-plugin-filesize';
+
+import pkg from './package.json';
+
+const input = path.join(__dirname, 'src', 'index.ts');
+const output = path.join(__dirname, 'dist');
+const banner = `
+/**
+  Version: ${pkg.version}
+**/`;
+
+const formats = ['umd', 'es'];
 
 export default {
-  input: path.join(__dirname, "src", "index.ts"),
-  output: {
-    name: "JSONApiClient",
-    file: path.join(__dirname, "dist", "index.js"),
-    format: "umd",
-    sourcemap: true,
-    globals: {
-      axios: "axios"
-    }
-  },
-  external: ["axios"],
+  input: input,
+  output: formats.map((format) => {
+    return {
+      name: 'JSONApiClient',
+      file: path.join(output, `index.${format}.js`),
+      format: format,
+      sourcemap: true,
+      banner: banner,
+      globals: {
+        axios: 'axios'
+      }
+    };
+  }),
+  external: ['axios'],
   plugins: [
     typescript({
       tsconfigOverride: {
         compilerOptions: {
-          module: "ES2015"
+          module: 'ES2015'
         }
       }
     }),
     filesize()
-  ],
-  watch: {
-    include: "src/**/*.ts"
-  }
+  ]
 };
